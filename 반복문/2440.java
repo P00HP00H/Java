0a1a7df33f48fb89bd29db37da5c2d8e43a11f69
P00HP00H <k5602o@nate.com>
2019-03-04 23:40:18 +0900

    백준 2440번 : 별찍기3 --> for문

---
+++ 반복문/2440.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		int input;
+		Scanner scan = new Scanner(System.in);
+		input = scan.nextInt();
+		
+		for(int i=0; i<input; i++) {
+			for(int j=0; j<input-i; j++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+	}
+}
