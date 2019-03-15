5aa72422e3d4231772e0d0652c1f4e6e56c3f2fd
P00HP00H <k5602o@nate.com>
2019-03-15 18:15:01 +0900

    백준 4344번 - %자리 소숫점 틀림 + 전역변수 꼬인듯(하나 입력은 답이 얼추 맞지만 모두 입력하면 아예 다 틀려버림)

--- 조건문/4344.java
+++ 조건문/4344.java
@@ -2,19 +2,35 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-	
+	    String[] output = null;		// 꼭 이렇게 해줘야하나봐... 안 그러면 오류남
+		int[] score;
 		int num3 = 0;
 		Scanner scan = new Scanner(System.in);
 		int input = scan.nextInt();
 		
 		for(int i=0; i<input; i++) {
 			int num = scan.nextInt();
+			score = new int[num];
 			for(int j=0; j<num; j++) {
 				int num2 = scan.nextInt();
+				score[j] = num2;
 				num3 += num2;
 			}
 			double num4 = num3 / num;
-			System.out.println("평균: "+num4);
+		
+			int chk=0;
+			for(int k=0; k<score.length; k++) {
+				if(score[k] > num4) {
+					chk += 1;
+				}
+			}
+			double total = (100 * chk) / score.length;
+			output = new String[input];
+			output[i] = String.format("%.3f", total)+"%";
+		}
+
+		for(int l=0; l<output.length; l++) {
+			System.out.println(output[l]);
 		}
 	}
 }
